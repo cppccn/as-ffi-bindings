535b0e5e7367a87a3645c09e7ecb97a7d40c460f
Adrien Zinger <zinger.ad@gmail.com>
2022-01-03 14:48:19 +0100

    Never panic, throw errors (#8)

--- src/tools.rs
+++ src/tools.rs
@@ -1,18 +1,35 @@
 use super::{Env, Read, StringPtr};
 
 // if get_string throws an exception abort for some reason is being called
-pub fn abort(env: &Env, message: StringPtr, filename: StringPtr, line: i32, col: i32) {
-    let memory = env.memory.get_ref().expect("initialized memory");
-    let message = message.read(memory).unwrap();
-    let filename = filename.read(memory).unwrap();
+pub fn abort(
+    env: &Env,
+    message: StringPtr,
+    filename: StringPtr,
+    line: i32,
+    col: i32,
+) -> Result<(), wasmer::RuntimeError> {
+    let memory = match env.memory.get_ref() {
+        Some(mem) => mem,
+        _ => return Err(wasmer::RuntimeError::new("Cannot get memory")),
+    };
+    let message = match message.read(memory) {
+        Ok(msg) => msg,
+        Err(err) => return Err(wasmer::RuntimeError::new(err.to_string())),
+    };
+    let filename = match filename.read(memory) {
+        Ok(filename) => filename,
+        Err(err) => return Err(wasmer::RuntimeError::new(err.to_string())),
+    };
     eprintln!("Error: {} at {}:{} col: {}", message, filename, line, col);
+    Ok(())
 }
 
 macro_rules! export_asr {
     ($func_name:ident, $env:expr) => {
-        $env.$func_name
-            .as_ref()
-            .expect("Assembly Script Runtime not exported")
+        match $env.$func_name.as_ref() {
+            Some(res) => res,
+            _ => anyhow::bail!("Failed to get func"),
+        }
     };
 }
 pub(crate) use export_asr;
