855a055f6f57c5c8405adcd8ec68028cc1573352
adrien-zinger <zinger.ad@gmail.com>
2022-06-29 12:15:29 +0000

    Deploying to gh-pages from @ cppccn/as-ffi-bindings@4ebdf6bfff67155b0c2d3ee86e283b35d5b585b5 🚀

--- as_ffi_bindings/sidebar-items.js
+++ as_ffi_bindings/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error",""],["Type",""]],"fn":[["abort",""]],"struct":[["AnyPtr",""],["AnyPtrExported",""],["BufferPtr",""],["Env",""],["StringPtr",""]],"trait":[["Read",""],["Write",""]]});
+window.SIDEBAR_ITEMS = {"enum":[["Error",""],["Type",""]],"fn":[["abort",""]],"struct":[["AnyPtr",""],["AnyPtrExported",""],["BufferPtr",""],["Env",""],["StringPtr",""]],"trait":[["Read",""],["Write",""]]};
